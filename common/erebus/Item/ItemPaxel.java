4c8c5bf92c7424d9c35a043165665158beeb5663
ProPercivalalb <propercivalalb@gmail.com>
2013-08-08 15:21:05 +0100

    Fixed paxel not working on mod blocks

--- common/erebus/Item/ItemPaxel.java
+++ common/erebus/Item/ItemPaxel.java
@@ -1,7 +1,11 @@
 package erebus.Item;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+
+import erebus.core.helper.ReflectionHelper;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.EnumToolMaterial;
@@ -19,26 +23,26 @@ public class ItemPaxel extends ItemTool {
 	
 	@Override
 	public boolean canHarvestBlock(Block par1Block) {
-	    return Item.axeDiamond.canHarvestBlock(par1Block) || Item.pickaxeDiamond.canHarvestBlock(par1Block) || Item.shovelDiamond.canHarvestBlock(par1Block);
+	    return Item.axeIron.canHarvestBlock(par1Block) || Item.pickaxeIron.canHarvestBlock(par1Block) || Item.shovelIron.canHarvestBlock(par1Block);
 	}
 	
 	@Override
     public float getStrVsBlock(ItemStack par1ItemStack, Block par2Block) {
-		//Creates a backup of the diamond tool efficiencies
-		float oldSpeedPickaxe = ((ItemTool)Item.pickaxeDiamond).efficiencyOnProperMaterial; 
-		float oldSpeedAxe = ((ItemTool)Item.axeDiamond).efficiencyOnProperMaterial;
-		float oldSpeedShovel = ((ItemTool)Item.shovelDiamond).efficiencyOnProperMaterial;
-		//Sets the diamond tools efficiencies to this classes efficiencies
-		((ItemTool)Item.pickaxeDiamond).efficiencyOnProperMaterial = this.efficiencyOnProperMaterial;
-		((ItemTool)Item.axeDiamond).efficiencyOnProperMaterial = this.efficiencyOnProperMaterial;
-		((ItemTool)Item.shovelDiamond).efficiencyOnProperMaterial = this.efficiencyOnProperMaterial;
-		float pickaxeSpeed = Item.pickaxeDiamond.getStrVsBlock(par1ItemStack, par2Block);
-		float axeSpeed = Item.axeDiamond.getStrVsBlock(par1ItemStack, par2Block);
-		float shovelSpeed = Item.shovelDiamond.getStrVsBlock(par1ItemStack, par2Block);
-		//Sets the diamond tools efficiencies back to what they were
-		((ItemTool)Item.pickaxeDiamond).efficiencyOnProperMaterial = oldSpeedPickaxe;
-		((ItemTool)Item.axeDiamond).efficiencyOnProperMaterial = oldSpeedAxe;
-		((ItemTool)Item.shovelDiamond).efficiencyOnProperMaterial = oldSpeedShovel;
+		//Creates a backup of the Iron tool efficiencies
+		float oldSpeedPickaxe = ((ItemTool)Item.pickaxeIron).efficiencyOnProperMaterial; 
+		float oldSpeedAxe = ((ItemTool)Item.axeIron).efficiencyOnProperMaterial;
+		float oldSpeedShovel = ((ItemTool)Item.shovelIron).efficiencyOnProperMaterial;
+		//Sets the Iron tools efficiencies to this classes efficiencies
+		((ItemTool)Item.pickaxeIron).efficiencyOnProperMaterial = this.efficiencyOnProperMaterial;
+		((ItemTool)Item.axeIron).efficiencyOnProperMaterial = this.efficiencyOnProperMaterial;
+		((ItemTool)Item.shovelIron).efficiencyOnProperMaterial = this.efficiencyOnProperMaterial;
+		float pickaxeSpeed = Item.pickaxeIron.getStrVsBlock(par1ItemStack, par2Block);
+		float axeSpeed = Item.axeIron.getStrVsBlock(par1ItemStack, par2Block);
+		float shovelSpeed = Item.shovelIron.getStrVsBlock(par1ItemStack, par2Block);
+		//Sets the Iron tools efficiencies back to what they were
+		((ItemTool)Item.pickaxeIron).efficiencyOnProperMaterial = oldSpeedPickaxe;
+		((ItemTool)Item.axeIron).efficiencyOnProperMaterial = oldSpeedAxe;
+		((ItemTool)Item.shovelIron).efficiencyOnProperMaterial = oldSpeedShovel;
 		if(pickaxeSpeed > 1.0F) return pickaxeSpeed;
 		if(axeSpeed > 1.0F) return axeSpeed;
 		if(shovelSpeed > 1.0F) return shovelSpeed;
@@ -47,15 +51,28 @@ public class ItemPaxel extends ItemTool {
 
 	@Override
 	public float getStrVsBlock(ItemStack stack, Block block, int meta)  {
-	    if (ForgeHooks.isToolEffective(stack, block, meta)) {
+	    if (isToolEffective(block, meta)) {
 	        return efficiencyOnProperMaterial;
 	    }
 	    return getStrVsBlock(stack, block);
 	}
 	
-	//TODO
-	//public static boolean isToolEffective(ItemStack stack, Block block, int metadata) {
-	    //List toolClass = ForgeHooks.toolClasses.get(stack.getItem());
-	    //return toolClass != null && toolEffectiveness.contains(Arrays.asList(block, metadata, toolClass.get(0)));
-	//}
+	public static boolean isToolEffective(Block block, int metadata) {
+        List toolClass = (List)ReflectionHelper.getField(ForgeHooks.class, HashMap.class, null, "toolClasses").get(Item.pickaxeIron);
+        if(toolClass != null) {
+        	return ReflectionHelper.getField(ForgeHooks.class, HashSet.class, null, "toolHarvestLevels").contains(Arrays.asList(block, metadata, toolClass.get(0)));
+        }
+        
+        toolClass = (List)ReflectionHelper.getField(ForgeHooks.class, HashMap.class, null, "toolClasses").get(Item.axeIron);
+        if(toolClass != null) {
+        	return ReflectionHelper.getField(ForgeHooks.class, HashSet.class, null, "toolHarvestLevels").contains(Arrays.asList(block, metadata, toolClass.get(0)));
+        }
+        
+        toolClass = (List)ReflectionHelper.getField(ForgeHooks.class, HashMap.class, null, "toolClasses").get(Item.shovelIron);
+        if(toolClass != null) {
+        	return ReflectionHelper.getField(ForgeHooks.class, HashSet.class, null, "toolHarvestLevels").contains(Arrays.asList(block, metadata, toolClass.get(0)));
+        }
+        
+        return false;
+    }
 }

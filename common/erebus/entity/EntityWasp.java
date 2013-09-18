11549d520360f1bfe81d47d7dad644303d4a04f2
Vadis365 <curve_linear@hotmail.com>
2013-09-18 01:31:45 +0100

    A few changes to the Wasp mob
    changed drops to exoskeleton & yellow dye / rare drop: Wasp sword
    changed attack strength
    changed entity to extend EntityMob
    Removed some unused methods

--- common/erebus/entity/EntityWasp.java
+++ common/erebus/entity/EntityWasp.java
@@ -1,133 +1,146 @@
 package erebus.entity;
 
-import erebus.client.render.entity.AnimationMathHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
+import ErebusNotIncluded.mobs.render.AnimationMathHelper;
+import erebus.ModItems;
 
-public class EntityWasp extends EntityUndergroundAnimal{
-	private float heightOffset=0.5F;
+public class EntityWasp extends EntityMob {
+	private float heightOffset = 0.5F;
 	public float wingFloat;
-	AnimationMathHelper mathWings=new AnimationMathHelper();
+	AnimationMathHelper mathWings = new AnimationMathHelper();
 
-	public EntityWasp(World par1World){
+	public EntityWasp(World par1World) {
 		super(par1World);
-		this.setSize(2.0F,2.0F);
+		this.setSize(2.0F, 2.0F);
 		// no AI needed - just seems to work o.k. being a 'monster'
 	}
 
-	protected boolean isAIEnabled(){
-		return false;
-	}
-
-	protected void entityInit(){
+	@Override
+	protected void entityInit() {
 		super.entityInit();
-		this.dataWatcher.addObject(16,new Byte((byte)0));
+		this.dataWatcher.addObject(16, new Byte((byte) 0));
 	}
 
 	@Override
-	protected void applyEntityAttributes() {
-	    super.applyEntityAttributes();
-	    getEntityAttribute(SharedMonsterAttributes.maxHealth).setAttribute(10.0D);
-	}
-
-	public int getAttackStrength(Entity par1Entity){
-		return 5;
-	}
-
-	public EnumCreatureAttribute getCreatureAttribute(){
+	public EnumCreatureAttribute getCreatureAttribute() {
 		return EnumCreatureAttribute.ARTHROPOD;
 	}
 
-	protected void fall(float par1){}
+	@Override
+	protected void fall(float par1) {
+	}
 
-	protected String getLivingSound(){
+	@Override
+	protected String getLivingSound() {
 		return "erebus:WaspSound";
 	}
 
-	protected String getHurtSound(){
+	@Override
+	protected String getHurtSound() {
 		return "erebus:WaspHurt";
 	}
 
-	protected String getDeathSound(){
+	@Override
+	protected String getDeathSound() {
 		return "erebus:squish";
 	}
 
-	protected void getStepSound(int par1, int par2, int par3, int par4){
-		this.worldObj.playSoundAtEntity(this,"mob.zombie.wood",0.15F,1.0F);
+	protected void getStepSound(int par1, int par2, int par3, int par4) {
+		this.worldObj.playSoundAtEntity(this, "mob.zombie.wood", 0.15F, 1.0F);
 	}
 
-	protected int getDropItemId(){
-		return Item.coal.itemID;
-	}
-
-	protected void dropRareDrop(int par1){
-		switch(this.rand.nextInt(2)){
-			case 0:
-				dropItem(Item.appleRed.itemID,1);
-			case 1:
-				dropItem(Item.goldNugget.itemID,1);
+	@Override
+	protected void dropFewItems(boolean par1, int par2) {
+		switch (this.rand.nextInt(2)) {
+		case 0:
+			this.entityDropItem(new ItemStack(ModItems.erebusMaterials,
+					this.rand.nextInt(3) + 1, 0), 0.0F);
+		case 1:
+			this.entityDropItem(
+					new ItemStack(Item.dyePowder, this.rand.nextInt(2) + 1, 11),
+					0.0F);
 		}
 	}
 
-	public boolean isFlying(){
+	@Override
+	protected void dropRareDrop(int par1) {
+
+		dropItem(ModItems.waspSwordID, 1);
+	}
+
+	public boolean isFlying() {
 		return !this.onGround;
 	}
 
-	public boolean isClimbing(){
-		return (!this.onGround)&&(isOnLadder());
-	}
-
-	public boolean isOnLadder(){
+	@Override
+	public boolean isOnLadder() {
 		return this.isCollidedHorizontally;
 	}
 
-	public void onUpdate(){
+	@Override
+	public void onUpdate() {
 		super.onUpdate();
-		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setAttribute(0.75D);
-		
-		if (!this.isFlying()){
-			this.wingFloat=0.0F;
+		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setAttribute(
+				0.75D); // Movespeed
+		getEntityAttribute(SharedMonsterAttributes.maxHealth).setAttribute(
+				25.0D); // Max Health
+		getEntityAttribute(SharedMonsterAttributes.attackDamage).setAttribute(
+				6.0D); // atkDmg
+		getEntityAttribute(SharedMonsterAttributes.followRange).setAttribute(
+				16.0D); // followRange
+
+		if (!this.isFlying()) {
+			this.wingFloat = 0.0F;
 		}
-		if (this.isFlying()){
-			this.wingFloat=mathWings.swing(4.0F,0.1F);
+		if (this.isFlying()) {
+			this.wingFloat = mathWings.swing(4.0F, 0.1F);
 		}
 	}
 
-	public void onLivingUpdate(){
-		if (!this.worldObj.isRemote){
-			this.heightOffset=(0.5F+(float)this.rand.nextGaussian()*5.0F);
-			if ((getEntityToAttack()!=null)&&(getEntityToAttack().posY+getEntityToAttack().getEyeHeight()>this.posY+getEyeHeight()+this.heightOffset)){
-				this.motionY+=(0.350000011920929D-this.motionY)*0.350000011920929D;
+	@Override
+	public void onLivingUpdate() {
+		if (!this.worldObj.isRemote) {
+			this.heightOffset = (0.5F + (float) this.rand.nextGaussian() * 5.0F);
+			if ((getEntityToAttack() != null)
+					&& (getEntityToAttack().posY
+							+ getEntityToAttack().getEyeHeight() > this.posY
+							+ getEyeHeight() + this.heightOffset)) {
+				this.motionY += (0.350000011920929D - this.motionY) * 0.350000011920929D;
 			}
 		}
-		if ((!this.onGround)&&(this.motionY<0.0D)){
-			this.motionY*=0.5D;
+		if ((!this.onGround) && (this.motionY < 0.0D)) {
+			this.motionY *= 0.5D;
 		}
 		super.onLivingUpdate();
 	}
 
-	public boolean attackEntityAsMob(Entity par1Entity){
-		if (super.attackEntityAsMob(par1Entity)){
-			if ((par1Entity instanceof EntityLivingBase)){
-				byte var2=0;
+	@Override
+	public boolean attackEntityAsMob(Entity par1Entity) {
+		if (super.attackEntityAsMob(par1Entity)) {
+			if ((par1Entity instanceof EntityLivingBase)) {
+				byte var2 = 0;
 
-				if (this.worldObj.difficultySetting>1){
-					if (this.worldObj.difficultySetting==2){
-						var2=7;
-					}
-					else if (this.worldObj.difficultySetting==3){
-						var2=15;
+				if (this.worldObj.difficultySetting > 1) {
+					if (this.worldObj.difficultySetting == 2) {
+						var2 = 7;
+					} else if (this.worldObj.difficultySetting == 3) {
+						var2 = 15;
 					}
 				}
 
-				if (var2>0){
-					((EntityLivingBase)par1Entity).addPotionEffect(new PotionEffect(Potion.poison.id,var2*20,0));
+				if (var2 > 0) {
+					((EntityLivingBase) par1Entity)
+							.addPotionEffect(new PotionEffect(Potion.poison.id,
+									var2 * 20, 0));
 				}
 			}
 
@@ -137,8 +150,11 @@ public class EntityWasp extends EntityUndergroundAnimal{
 		return false;
 	}
 
-	protected void attackEntity(Entity par1Entity, float par2){
-		if ((par2<2.0F)&&(par1Entity.boundingBox.maxY>this.boundingBox.minY)&&(par1Entity.boundingBox.minY<this.boundingBox.maxY)){
+	@Override
+	protected void attackEntity(Entity par1Entity, float par2) {
+		if ((par2 < 2.0F)
+				&& (par1Entity.boundingBox.maxY > this.boundingBox.minY)
+				&& (par1Entity.boundingBox.minY < this.boundingBox.maxY)) {
 			attackEntityAsMob(par1Entity);
 		}
 	}

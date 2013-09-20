8f246a6f898200ade4f68246ee2af3c95072d7f4
ganymedes01 <Andre@andre.home>
2013-09-20 12:13:37 +0100

    Finished (maybe) version of the Umber Furnace
    Also added a Utils class with a couple of usefull methods. Feel free to
    add as many more as you want.

--- common/erebus/client/gui/GuiUmberFurnace.java
+++ common/erebus/client/gui/GuiUmberFurnace.java
@@ -11,6 +11,7 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import erebus.inventory.ContainerUmberFurnace;
 import erebus.tileentity.TileEntityUmberFurnace;
+import erebus.utils.Utils;
 
 @SideOnly(Side.CLIENT)
 public class GuiUmberFurnace extends GuiContainer {
@@ -25,8 +26,9 @@ public class GuiUmberFurnace extends GuiContainer {
 
 	@Override
 	protected void drawGuiContainerForegroundLayer(int par1, int par2) {
-		fontRenderer.drawString(I18n.getString(furnace.getInvName()), 8, 6, 4210752);
-		fontRenderer.drawString(I18n.getString("container.inventory"), 8, ySize - 96 + 2, 4210752);
+		int color = Utils.getColour(255, 255, 255);
+		fontRenderer.drawString(I18n.getString(furnace.getInvName()), xSize / 2 - fontRenderer.getStringWidth(I18n.getString(furnace.getInvName())) / 2, 6, color);
+		fontRenderer.drawString(I18n.getString("container.inventory"), xSize - 60, ySize - 96 + 2, color, true);
 	}
 
 	@Override
@@ -36,7 +38,16 @@ public class GuiUmberFurnace extends GuiContainer {
 		int k = (width - xSize) / 2;
 		int l = (height - ySize) / 2;
 		drawTexturedModalRect(k, l, 0, 0, xSize, ySize);
-		int i1 = furnace.getScaledFluidAmount(65);
-		drawTexturedModalRect(k + 10, l + 75 - i1, 176, 96 - i1, 18, i1);
+
+		int i1;
+		if (furnace.isBurning()) {
+			i1 = furnace.getBurnTimeRemainingScaled(12);
+			drawTexturedModalRect(k + 56, l + 36 + 12 - i1, 175, 12 - i1, 14, i1 + 2);
+		}
+		i1 = furnace.getCookProgressScaled(24);
+		drawTexturedModalRect(k + 79, l + 35, 176, 14, i1 + 1, 16);
+
+		int size = furnace.getScaledFluidAmount(65);
+		drawTexturedModalRect(k + 10, l + 75 - size, 176, 96 - size, 18, size);
 	}
 }

a1d7fae7f79e7ba766a6cb432f89bd762c833e69
ganymedes01 <Andre@andre.home>
2013-09-20 14:41:15 +0100

    Added version check

--- common/erebus/lib/Reference.java
+++ common/erebus/lib/Reference.java
@@ -9,8 +9,11 @@ public class Reference {
 	public static final String MOD_ID = "Erebus";
 	public static final String MOD_NAME = "Erebus";
 	public static final String MOD_VERSION = "v0.1 for MC 1.6.2";
+	public static final int RAW_VERSION_NUMBER = 01;
+	public static String LATEST_VERSION;
 	public static final String MOD_DEPENDENCIES = "required-after:Forge@[9.10.0.819,)";
 	public static final String SP_CLIENT = "erebus.core.proxy.ClientProxy";
 	public static final String SP_SERVER = "erebus.core.proxy.CommonProxy";
+	public static final String VERSION_CHECK_FILE = "https://raw.github.com/ganymedes01/Version-Checks/master/Version.xml";
 	public static final String CHANNEL = "erebus";
 }

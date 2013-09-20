37e5a30171f0e04c01a0ff72115ff921d491f36d
ganymedes01 <Andre@andre.home>
2013-09-20 14:42:07 +0100

    ...

--- common/erebus/lib/Reference.java
+++ common/erebus/lib/Reference.java
@@ -14,6 +14,6 @@ public class Reference {
 	public static final String MOD_DEPENDENCIES = "required-after:Forge@[9.10.0.819,)";
 	public static final String SP_CLIENT = "erebus.core.proxy.ClientProxy";
 	public static final String SP_SERVER = "erebus.core.proxy.CommonProxy";
-	public static final String VERSION_CHECK_FILE = "https://raw.github.com/ganymedes01/Version-Checks/master/Version.xml";
+	public static final String VERSION_CHECK_FILE = "https://raw.github.com/ProPercivalalb/ErebusMod/master/VersionFile.xml";
 	public static final String CHANNEL = "erebus";
 }

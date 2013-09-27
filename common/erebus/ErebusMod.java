0cb88cce75af10f4cee366bba6dd2e8ad6f80c77
Vadis365 <curve_linear@hotmail.com>
2013-09-27 23:40:15 +0100

    The start of adding antlion mob

--- common/erebus/ErebusMod.java
+++ common/erebus/ErebusMod.java
@@ -1,6 +1,7 @@
 package erebus;
 
 import java.io.File;
+
 import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.EnumToolMaterial;
 import net.minecraftforge.common.DimensionManager;
@@ -30,6 +31,7 @@ import erebus.creativetab.CreativeTabErebus;
 import erebus.creativetab.CreativeTabErebusBlock;
 import erebus.creativetab.CreativeTabErebusGear;
 import erebus.creativetab.CreativeTabErebusItem;
+import erebus.entity.EntityAntlion;
 import erebus.lib.Reference;
 import erebus.network.PacketHandler;
 import erebus.recipes.BCFacadeManager;
@@ -41,76 +43,77 @@ import erebus.world.WorldProviderErebus;
 @NetworkMod(channels = { Reference.CHANNEL }, clientSideRequired = true, serverSideRequired = true, packetHandler = PacketHandler.class)
 public class ErebusMod {
 
-	@SidedProxy(clientSide = Reference.SP_CLIENT, serverSide = Reference.SP_SERVER)
-	public static CommonProxy proxy;
+    @SidedProxy(clientSide = Reference.SP_CLIENT, serverSide = Reference.SP_SERVER)
+    public static CommonProxy proxy;
 
-	@Instance(Reference.MOD_ID)
-	public static ErebusMod instance;
+    @Instance(Reference.MOD_ID)
+    public static ErebusMod instance;
 
-	public static EnumArmorMaterial armorEXOSKELETON = EnumHelper.addArmorMaterial("EXOSKELETON", 11, new int[] { 2, 4, 3, 2 }, 15);
-	public static EnumArmorMaterial armorJADE = EnumHelper.addArmorMaterial("JADE", 24, new int[] { 3, 7, 5, 2 }, 15);
-	public static EnumToolMaterial toolJADE = EnumHelper.addToolMaterial("JADE", 2, 863, 10.0F, 2.0F, 18);
-	public static EnumToolMaterial toolJADEPAXEL = EnumHelper.addToolMaterial("JADEPAXEL", 2, 1079, 8.0F, 4.0F, 14);
-	public static EnumToolMaterial toolCAVEMANCLUB = EnumHelper.addToolMaterial("CAVEMANCLUB", 0, 131, 4.0F, 2.0F, 12);
+    public static EnumArmorMaterial armorEXOSKELETON = EnumHelper.addArmorMaterial("EXOSKELETON", 11, new int[] { 2, 4, 3, 2 }, 15);
+    public static EnumArmorMaterial armorJADE = EnumHelper.addArmorMaterial("JADE", 24, new int[] { 3, 7, 5, 2 }, 15);
+    public static EnumToolMaterial toolJADE = EnumHelper.addToolMaterial("JADE", 2, 863, 10.0F, 2.0F, 18);
+    public static EnumToolMaterial toolJADEPAXEL = EnumHelper.addToolMaterial("JADEPAXEL", 2, 1079, 8.0F, 4.0F, 14);
+    public static EnumToolMaterial toolCAVEMANCLUB = EnumHelper.addToolMaterial("CAVEMANCLUB", 0, 131, 4.0F, 2.0F, 12);
 
-	public static CreativeTabErebus tabErebusBlock = new CreativeTabErebusBlock("erebus.block");
-	public static CreativeTabErebus tabErebusItem = new CreativeTabErebusItem("erebus.item");
-	public static CreativeTabErebus tabErebusGear = new CreativeTabErebusGear("erebus.gear");
+    public static CreativeTabErebus tabErebusBlock = new CreativeTabErebusBlock("erebus.block");
+    public static CreativeTabErebus tabErebusItem = new CreativeTabErebusItem("erebus.item");
+    public static CreativeTabErebus tabErebusGear = new CreativeTabErebusGear("erebus.gear");
 
-	public static int erebusDimensionID;
-	public static boolean activateExtraOres = false;
-	public static boolean shouldDoVersionCheck = true;
-	public static byte beetleLarvaEating = 0;
+    public static int erebusDimensionID;
+    public static boolean activateExtraOres = false;
+    public static boolean shouldDoVersionCheck = true;
+    public static byte beetleLarvaEating = 0;
 
-	public static ConnectionTeleportHandler packeterebushandler = new ConnectionTeleportHandler();
+    public static ConnectionTeleportHandler packeterebushandler = new ConnectionTeleportHandler();
 
-	@EventHandler
-	@SideOnly(Side.CLIENT)
-	public void preInitClient(FMLPreInitializationEvent event) {
-		MinecraftForge.EVENT_BUS.register(new EntitySoundEvent());
-		AmbientMusicManager.register();
+    @EventHandler
+    @SideOnly(Side.CLIENT)
+    public void preInitClient(FMLPreInitializationEvent event) {
+	MinecraftForge.EVENT_BUS.register(new EntitySoundEvent());
+	AmbientMusicManager.register();
+    }
+
+    @EventHandler
+    public void preInitServer(FMLPreInitializationEvent event) {
+	LogHelper.init();
+
+	ConfigurationHandler.loadConfig(new File(event.getModConfigurationDirectory().getAbsolutePath() + File.separator + Reference.MOD_ID + ".cfg"));
+
+	if (shouldDoVersionCheck) {
+	    VersionHelper.execute();
+	    TickRegistry.registerTickHandler(new VersionCheckTickHandler(), Side.CLIENT);
 	}
 
-	@EventHandler
-	public void preInitServer(FMLPreInitializationEvent event) {
-		LogHelper.init();
+	ModBlocks.init();
+	ModItems.init();
+	ModEntities.init();
 
-		ConfigurationHandler.loadConfig(new File(event.getModConfigurationDirectory().getAbsolutePath() + File.separator + Reference.MOD_ID + ".cfg"));
+	NetworkRegistry.instance().registerConnectionHandler(packeterebushandler);
+	NetworkRegistry.instance().registerGuiHandler(instance, proxy);
 
-		if (shouldDoVersionCheck) {
-			VersionHelper.execute();
-			TickRegistry.registerTickHandler(new VersionCheckTickHandler(), Side.CLIENT);
-		}
+	DimensionManager.registerProviderType(erebusDimensionID, WorldProviderErebus.class, true);
+	DimensionManager.registerDimension(erebusDimensionID, erebusDimensionID);
+    }
 
-		ModBlocks.init();
-		ModItems.init();
-		ModEntities.init();
+    @EventHandler
+    public void load(FMLInitializationEvent event) {
+	proxy.registerTileEntities();
+	ModBiomes.init();
+	proxy.registerRenderInformation();
+	RecipeHandler.init();
 
-		NetworkRegistry.instance().registerConnectionHandler(packeterebushandler);
-		NetworkRegistry.instance().registerGuiHandler(instance, proxy);
+	MinecraftForge.EVENT_BUS.register(ModBlocks.erebusSapling);
+	MinecraftForge.EVENT_BUS.register(ModBlocks.quickSand);
+	MinecraftForge.EVENT_BUS.register(ModBlocks.insectRepellent);
+	MinecraftForge.EVENT_BUS.register(ModItems.jumpBoots);
+	MinecraftForge.EVENT_BUS.register(new EntityAntlion(null));
 
-		DimensionManager.registerProviderType(erebusDimensionID, WorldProviderErebus.class, true);
-		DimensionManager.registerDimension(erebusDimensionID, erebusDimensionID);
-	}
+	TickRegistry.registerTickHandler(new CommonTickHandler(), Side.SERVER);
+	BCFacadeManager.registerFacades();
+    }
 
-	@EventHandler
-	public void load(FMLInitializationEvent event) {
-		proxy.registerTileEntities();
-		ModBiomes.init();
-		proxy.registerRenderInformation();
-		RecipeHandler.init();
-
-		MinecraftForge.EVENT_BUS.register(ModBlocks.erebusSapling);
-		MinecraftForge.EVENT_BUS.register(ModBlocks.quickSand);
-		MinecraftForge.EVENT_BUS.register(ModBlocks.insectRepellent);
-		MinecraftForge.EVENT_BUS.register(ModItems.jumpBoots);
-
-		TickRegistry.registerTickHandler(new CommonTickHandler(), Side.SERVER);
-		BCFacadeManager.registerFacades();
-	}
-
-	@EventHandler
-	public void postLoad(FMLPostInitializationEvent event) {
-		proxy.postLoad();
-	}
+    @EventHandler
+    public void postLoad(FMLPostInitializationEvent event) {
+	proxy.postLoad();
+    }
 }
--- common/erebus/core/proxy/ClientProxy.java
+++ common/erebus/core/proxy/ClientProxy.java
@@ -14,7 +14,9 @@ import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraft.world.World;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
+
 import com.google.common.io.ByteArrayDataInput;
+
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.registry.TickRegistry;
@@ -22,6 +24,7 @@ import cpw.mods.fml.relauncher.Side;
 import erebus.ModBlocks;
 import erebus.ModItems;
 import erebus.client.fx.EntityRepellentFX;
+import erebus.client.model.entity.ModelAntlion;
 import erebus.client.model.entity.ModelBeetle;
 import erebus.client.model.entity.ModelBeetleLarva;
 import erebus.client.model.entity.ModelBlackWidow;
@@ -38,6 +41,7 @@ import erebus.client.model.entity.ModelTarantula;
 import erebus.client.model.entity.ModelVelvetWorm;
 import erebus.client.model.entity.ModelWasp;
 import erebus.client.render.EntityRendererErebus;
+import erebus.client.render.entity.RenderAntlion;
 import erebus.client.render.entity.RenderBeetle;
 import erebus.client.render.entity.RenderBeetleLarva;
 import erebus.client.render.entity.RenderBlackWidow;
@@ -63,6 +67,7 @@ import erebus.client.render.tileentity.TileEntityRenderHollowLog;
 import erebus.client.render.tileentity.TileEntitySpawnerRender;
 import erebus.core.handler.ClientTickHandler;
 import erebus.core.handler.PortalOverlayHandler;
+import erebus.entity.EntityAntlion;
 import erebus.entity.EntityBeetle;
 import erebus.entity.EntityBeetleLarva;
 import erebus.entity.EntityBlackWidow;
@@ -85,90 +90,90 @@ import erebus.tileentity.TileEntityHollowLog;
 import erebus.tileentity.TileEntitySpawner;
 
 public class ClientProxy extends CommonProxy {
-	public static EntityRendererErebus renderer;
+    public static EntityRendererErebus renderer;
 
-	@Override
-	public void registerRenderInformation() {
+    @Override
+    public void registerRenderInformation() {
 
-		MinecraftForge.EVENT_BUS.register(new PortalOverlayHandler());
-		TickRegistry.registerTickHandler(new ClientTickHandler(), Side.CLIENT);
-		RenderingRegistry.registerEntityRenderingHandler(EntityBeetle.class, new RenderBeetle(new ModelBeetle(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityFly.class, new RenderFly());
-		// RenderingRegistry.registerEntityRenderingHandler(EntityGreenfly.class,
-		// new RenderGreenfly(new ModelGreenfly(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityTarantula.class, new RenderTarantula(new ModelTarantula(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityMosquito.class, new RenderMosquito(new ModelMosquito(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityVelvetWorm.class, new RenderVelvetWorm(new ModelVelvetWorm(), 0.6F, 1.0F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityWasp.class, new RenderWasp(new ModelWasp(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityCentipede.class, new RenderCentipede(new ModelCentipede(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityBeetleLarva.class, new RenderBeetleLarva(new ModelBeetleLarva(), 0.5F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityBotFly.class, new RenderBotFly(new ModelBotFly(), 0.3F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityBlackWidow.class, new RenderBlackWidow(new ModelBlackWidow(), 0.3F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityScorpion.class, new RenderScorpion(new ModelScorpion(), 0.3F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityGrasshopper.class, new RenderGrasshopper(new ModelGrasshopper(), 0.3F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityLocust.class, new RenderLocust(new ModelLocust(), 0.3F));
-		RenderingRegistry.registerEntityRenderingHandler(EntitySolifuge.class, new RenderSolifuge(new ModelSolifuge(), 0.3F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityMoth.class, new RenderMoth(new ModelMoth(), 0.3F));
-		RenderingRegistry.registerEntityRenderingHandler(EntityFirebrat.class, new RenderFirebrat(new ModelFirebrat(), 0.3F));
-
-		// Special Renderer
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityHollowLog.class, new TileEntityRenderHollowLog());
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBambooCrate.class, new TileEntityRenderBamboo());
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEntitySpawner.class, new TileEntitySpawnerRender());
-
-		MinecraftForgeClient.registerItemRenderer(ModBlocks.hollowLogAcacia.blockID, new HollowLogItemRenderer(TileEntityRenderHollowLog.hollowLogResource));
-		MinecraftForgeClient.registerItemRenderer(ModBlocks.bambooCrate.blockID, new BambooItemRenderer());
-		MinecraftForgeClient.registerItemRenderer(ModItems.waspSword.itemID, new WaspSwordItemRenderer());
-		MinecraftForgeClient.registerItemRenderer(ModBlocks.umberFurnace.blockID, new ItemUmberFurnaceRenderer());
-	}
+	MinecraftForge.EVENT_BUS.register(new PortalOverlayHandler());
+	TickRegistry.registerTickHandler(new ClientTickHandler(), Side.CLIENT);
+	RenderingRegistry.registerEntityRenderingHandler(EntityBeetle.class, new RenderBeetle(new ModelBeetle(), 0.5F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityFly.class, new RenderFly());
+	// RenderingRegistry.registerEntityRenderingHandler(EntityGreenfly.class,
+	// new RenderGreenfly(new ModelGreenfly(), 0.5F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityTarantula.class, new RenderTarantula(new ModelTarantula(), 0.5F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityMosquito.class, new RenderMosquito(new ModelMosquito(), 0.5F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityVelvetWorm.class, new RenderVelvetWorm(new ModelVelvetWorm(), 0.6F, 1.0F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityWasp.class, new RenderWasp(new ModelWasp(), 0.5F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityCentipede.class, new RenderCentipede(new ModelCentipede(), 0.5F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityBeetleLarva.class, new RenderBeetleLarva(new ModelBeetleLarva(), 0.5F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityBotFly.class, new RenderBotFly(new ModelBotFly(), 0.3F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityBlackWidow.class, new RenderBlackWidow(new ModelBlackWidow(), 0.3F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityScorpion.class, new RenderScorpion(new ModelScorpion(), 0.3F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityGrasshopper.class, new RenderGrasshopper(new ModelGrasshopper(), 0.3F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityLocust.class, new RenderLocust(new ModelLocust(), 0.3F));
+	RenderingRegistry.registerEntityRenderingHandler(EntitySolifuge.class, new RenderSolifuge(new ModelSolifuge(), 0.3F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityMoth.class, new RenderMoth(new ModelMoth(), 0.3F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityFirebrat.class, new RenderFirebrat(new ModelFirebrat(), 0.3F));
+	RenderingRegistry.registerEntityRenderingHandler(EntityAntlion.class, new RenderAntlion(new ModelAntlion(), 0.3F));
 	
-	@Override
-	public void postLoad() {
-		Minecraft.getMinecraft().entityRenderer = renderer = new EntityRendererErebus();
+	// Special Renderer
+	ClientRegistry.bindTileEntitySpecialRenderer(TileEntityHollowLog.class, new TileEntityRenderHollowLog());
+	ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBambooCrate.class, new TileEntityRenderBamboo());
+	ClientRegistry.bindTileEntitySpecialRenderer(TileEntitySpawner.class, new TileEntitySpawnerRender());
+
+	MinecraftForgeClient.registerItemRenderer(ModBlocks.hollowLogAcacia.blockID, new HollowLogItemRenderer(TileEntityRenderHollowLog.hollowLogResource));
+	MinecraftForgeClient.registerItemRenderer(ModBlocks.bambooCrate.blockID, new BambooItemRenderer());
+	MinecraftForgeClient.registerItemRenderer(ModItems.waspSword.itemID, new WaspSwordItemRenderer());
+	MinecraftForgeClient.registerItemRenderer(ModBlocks.umberFurnace.blockID, new ItemUmberFurnaceRenderer());
+    }
+
+    @Override
+    public void postLoad() {
+	Minecraft.getMinecraft().entityRenderer = renderer = new EntityRendererErebus();
+    }
+
+    @Override
+    public void setClientNightVision(boolean enable){
+	renderer.hasNightVisionEffect = enable;
+
+	if (Minecraft.getMinecraft().entityRenderer != renderer)
+	    Minecraft.getMinecraft().entityRenderer = renderer;
+    }
+
+    @Override
+    public void handleParticlePacket(INetworkManager manager, Packet250CustomPayload packet, EntityPlayer player, ByteArrayDataInput data) {
+	EffectRenderer eff = Minecraft.getMinecraft().effectRenderer;
+	byte particleType = data.readByte();
+
+	if (particleType == PacketParticle.BEETLE_LARVA_SQUISH) {
+	    EntityLivingBase e = (EntityLivingBase) player.worldObj.getEntityByID(data.readInt());
+
+	    for (int countparticles = 0; countparticles <= 200; ++countparticles)
+		eff.addEffect(new EntityBreakingFX(player.worldObj, e.posX + (e.getRNG().nextDouble() - 0.5D) * e.width, e.posY + e.getRNG().nextDouble() * e.height - e.yOffset, e.posZ + (e.getRNG().nextDouble() - 0.5D) * e.width, Item.slimeBall));
+	} else if (particleType == PacketParticle.BEETLE_LARVA_EAT || particleType == PacketParticle.GRASSHOPPER_EAT) { // x,y,z,blockID,meta
+	    EntityLivingBase e = (EntityLivingBase) player.worldObj.getEntityByID(data.readInt());
+	    int woodX = data.readInt(), woodY = data.readInt(), woodZ = data.readInt();
+	    Block block = Block.blocksList[data.readInt()];
+	    int blockMeta = data.readByte();
+
+	    if (e == null || block == null)
+		return;
+
+	    for (int countparticles = 0; countparticles <= 50; ++countparticles)
+		eff.addEffect(new EntityDiggingFX(player.worldObj, woodX + 0.5D + (e.getRNG().nextDouble() - 0.5D) * e.width, woodY + 0.2D + e.getRNG().nextDouble() * e.height - e.yOffset, woodZ + 0.5D + (e.getRNG().nextDouble() - 0.5D) * e.width, e.getRNG().nextGaussian() * 0.5D, e.getRNG()
+			.nextGaussian() * 0.01D, e.getRNG().nextGaussian() * 0.5D, block, blockMeta));
 	}
-	
-	@Override
-	public void setClientNightVision(boolean enable){
-		renderer.hasNightVisionEffect = enable;
-		
-		if (Minecraft.getMinecraft().entityRenderer != renderer) {
-			Minecraft.getMinecraft().entityRenderer = renderer;
-		}
-	}
+    }
 
-	@Override
-	public void handleParticlePacket(INetworkManager manager, Packet250CustomPayload packet, EntityPlayer player, ByteArrayDataInput data) {
-		EffectRenderer eff = Minecraft.getMinecraft().effectRenderer;
-		byte particleType = data.readByte();
+    @Override
+    public void spawnCustomParticle(String particleName, World world, double x, double y, double z, double vecX, double vecY, double vecZ) {
+	EntityFX fx = null;
 
-		if (particleType == PacketParticle.BEETLE_LARVA_SQUISH) {
-			EntityLivingBase e = (EntityLivingBase) player.worldObj.getEntityByID(data.readInt());
+	if (particleName.equals("repellent"))
+	    fx = new EntityRepellentFX(world, x, y, z, 0.0F, 0.0F, 0.0F);
 
-			for (int countparticles = 0; countparticles <= 200; ++countparticles)
-				eff.addEffect(new EntityBreakingFX(player.worldObj, e.posX + (e.getRNG().nextDouble() - 0.5D) * e.width, e.posY + e.getRNG().nextDouble() * e.height - e.yOffset, e.posZ + (e.getRNG().nextDouble() - 0.5D) * e.width, Item.slimeBall));
-		} else if (particleType == PacketParticle.BEETLE_LARVA_EAT || particleType == PacketParticle.GRASSHOPPER_EAT) { // x,y,z,blockID,meta
-			EntityLivingBase e = (EntityLivingBase) player.worldObj.getEntityByID(data.readInt());
-			int woodX = data.readInt(), woodY = data.readInt(), woodZ = data.readInt();
-			Block block = Block.blocksList[data.readInt()];
-			int blockMeta = data.readByte();
-
-			if (e == null || block == null)
-				return;
-
-			for (int countparticles = 0; countparticles <= 50; ++countparticles)
-				eff.addEffect(new EntityDiggingFX(player.worldObj, woodX + 0.5D + (e.getRNG().nextDouble() - 0.5D) * e.width, woodY + 0.2D + e.getRNG().nextDouble() * e.height - e.yOffset, woodZ + 0.5D + (e.getRNG().nextDouble() - 0.5D) * e.width, e.getRNG().nextGaussian() * 0.5D, e.getRNG()
-				.nextGaussian() * 0.01D, e.getRNG().nextGaussian() * 0.5D, block, blockMeta));
-		}
-	}
-
-	@Override
-	public void spawnCustomParticle(String particleName, World world, double x, double y, double z, double vecX, double vecY, double vecZ) {
-		EntityFX fx = null;
-
-		if (particleName.equals("repellent"))
-			fx = new EntityRepellentFX(world, x, y, z, 0.0F, 0.0F, 0.0F);
-
-		if (fx != null)
-			Minecraft.getMinecraft().effectRenderer.addEffect(fx);
-	}
+	if (fx != null)
+	    Minecraft.getMinecraft().effectRenderer.addEffect(fx);
+    }
 }

1c36ddb1de1f4bf86eb5326bcf46b89f876cad44
ganymedes01 <Andre@pc-193-218.customer.stchris.uod.lan>
2013-09-29 00:01:14 +0100

    Revert "Adding pick..."
    This reverts commit 63de986fa1e07c94ba7fe840d3997cbd871a926f.

--- common/erebus/ModItems.java
+++ common/erebus/ModItems.java
@@ -25,7 +25,6 @@ import erebus.item.ItemMaxSpeedBow;
 import erebus.item.ItemMetalIngots;
 import erebus.item.ItemPaxel;
 import erebus.item.ItemPortalActivator;
-import erebus.item.ItemSpecialPickaxe;
 import erebus.item.ItemSprayCan;
 import erebus.item.ItemSprintLeggings;
 
@@ -103,8 +102,7 @@ public class ModItems {
 
 		fossilClub = new ItemCavemanClub(fossilClubID, ErebusMod.toolCAVEMANCLUB).setFull3D().setUnlocalizedName("clubBone").setTextureName("erebus:clubBone");
 		waspSword = new ItemSword(waspSwordID, ErebusMod.toolJADE).setCreativeTab(null).setUnlocalizedName("waspSword");
-		maxSpeedBow = new ItemMaxSpeedBow(maxSpeedBowID).setUnlocalizedName("maxSpeedBow").setTextureName("erebus:maxSpeedBow");
-		specialPickaxe = new ItemSpecialPickaxe(specialPickaxeID).setUnlocalizedName("specialPickaxe").setTextureName("erebus:specialPickaxe");
+		maxSpeedBow = new ItemMaxSpeedBow(maxSpeedBowID, 301, 5).setUnlocalizedName("maxSpeedBow").setTextureName("erebus:maxSpeedBow");
 
 		compoundGoggles = new ItemCompoundGoggles(compoundGogglesID, ErebusMod.armorEXOSKELETON, 2, 0).setUnlocalizedName("compoundGoggles").setTextureName("erebus:compoundGoggles");
 		sprintLeggings = new ItemSprintLeggings(sprintLeggingsID, ErebusMod.armorEXOSKELETON, 2).setUnlocalizedName("sprintLeggings").setTextureName("erebus:sprintLeggings");
@@ -155,7 +153,6 @@ public class ModItems {
 		GameRegistry.registerItem(fossilClub, "erebus.clubBone");
 		GameRegistry.registerItem(waspSword, "erebus.waspSword");
 		GameRegistry.registerItem(maxSpeedBow, "erebus.maxSpeedBow");
-		GameRegistry.registerItem(specialPickaxe, "erebus.specialPickaxe");
 
 		GameRegistry.registerItem(compoundGoggles, "erebus.compoundGoggles");
 		GameRegistry.registerItem(sprintLeggings, "erebus.sprintLeggings");

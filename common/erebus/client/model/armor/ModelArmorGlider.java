01b158f33a88a16fd7afa779a6bc3fe26109733c
Vadis365 <curve_linear@hotmail.com>
2013-10-21 23:12:14 +0100

    I really don't know what I am doing anymore

--- common/erebus/client/model/armor/ModelArmorGlider.java
+++ common/erebus/client/model/armor/ModelArmorGlider.java
@@ -6,6 +6,8 @@ import net.minecraft.entity.Entity;
 
 import org.lwjgl.opengl.GL11;
 
+import erebus.item.ItemArmorGlider;
+
 public class ModelArmorGlider extends ModelBiped {
 	//fields
 	ModelRenderer Body;
@@ -93,6 +95,13 @@ public class ModelArmorGlider extends ModelBiped {
 	public void setRotationAngles(float par1, float par2, float par3, float par4, float par5, float par6, Entity par7Entity) {
 		super.setRotationAngles(par1, par2, par3, par4, par5, par6, par7Entity);
 
+		if (!ItemArmorGlider.gliding) {
+			RWing.rotateAngleZ = 0F;
+			LWing.rotateAngleZ = 0F;
+		}
+		else {
+			RWing.rotateAngleZ = 1.570796F;
+			LWing.rotateAngleZ = -1.570796F;
+		}
 	}
-
 }

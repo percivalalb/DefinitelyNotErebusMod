12ee52d9de0d1e61f31426e23db6325696569b86
ganymedes02 <Andre@andre.home>
2013-11-07 22:18:22 +0000

    Hidey Hidey
    You're going to need to add NotEnoughItems to the class path!

---
+++ common/erebus/integration/NEIErebusConfig.java
@@ -0,0 +1,28 @@
+package erebus.integration;
+
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+import erebus.ModBlocks;
+import erebus.lib.Reference;
+
+public class NEIErebusConfig implements IConfigureNEI {
+
+	@Override
+	public void loadConfig() {
+		API.hideItem(ModBlocks.erebusGrass.blockID);
+		API.hideItem(ModBlocks.portalErebus.blockID);
+		API.hideItem(ModBlocks.blockTurnip.blockID);
+		API.hideItem(ModBlocks.insectRepellent.blockID);
+		API.hideItem(ModBlocks.umberFurnace_on.blockID);
+	}
+
+	@Override
+	public String getName() {
+		return Reference.MOD_NAME;
+	}
+
+	@Override
+	public String getVersion() {
+		return Reference.MOD_VERSION;
+	}
+}

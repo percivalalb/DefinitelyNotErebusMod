21e284dbb7d74c89bd8403601ebc78783eb74bcc
Vadis365 <curve_linear@hotmail.com>
2013-11-17 22:26:28 +0000

    More mob tweakypoos

--- common/erebus/entity/EntitySolifuge.java
+++ common/erebus/entity/EntitySolifuge.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
@@ -16,6 +15,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import erebus.ModItems;
+import erebus.entity.ai.IEntityAIAttackOnCollide;
 import erebus.item.ItemErebusMaterial;
 
 public class EntitySolifuge extends EntityMob {
@@ -27,7 +27,7 @@ public class EntitySolifuge extends EntityMob {
 		isImmuneToFire = true;
 		setSize(2.0F, 1.0F);
 		tasks.addTask(0, new EntityAISwimming(this));
-		tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 0.3D, false));
+		tasks.addTask(1, new IEntityAIAttackOnCollide(this, EntityPlayer.class, 0.3D, false));
 		tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
 		tasks.addTask(3, new EntityAIWander(this, 0.3D));
 		targetTasks.addTask(0, new EntityAIHurtByTarget(this, false));
@@ -102,7 +102,7 @@ public class EntitySolifuge extends EntityMob {
 		if (rand.nextInt(8)<=2+(looting>>1))entityDropItem(new ItemStack(ModItems.erebusMaterials, 1, ItemErebusMaterial.dataBioVelocity), 0.0F);
 		entityDropItem(new ItemStack(ModItems.erebusMaterials, rand.nextInt(3) + 1, ItemErebusMaterial.dataExoPlate), 0.0F);
 	}
-	
+
 	@Override
 	protected void dropRareDrop(int looting){
 		entityDropItem(new ItemStack(ModItems.erebusMaterials, 1, ItemErebusMaterial.dataSupernaturalVelocity), 0.0F);

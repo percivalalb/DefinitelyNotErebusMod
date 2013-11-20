a29c312948e85d5cbe704e07b08934d1e8dce0b4
Vadis365 <curve_linear@hotmail.com>
2013-11-20 02:36:40 +0000

    Added new Bombardier model

--- common/erebus/entity/EntityBombardierBeetle.java
+++ common/erebus/entity/EntityBombardierBeetle.java
@@ -27,7 +27,7 @@ public class EntityBombardierBeetle extends EntityMob{
 		super(par1World);
 		moveSpeed = 1.0D;
 		stepHeight = 1.0F;
-		setSize(2.5F, 1.0F);
+		setSize(2.0F, 1.3F);
 		tasks.addTask(0, new EntityAISwimming(this));
 		tasks.addTask(1, new EntityAIExplodeAttackOnCollide(this, EntityPlayer.class, 0.3D, false));
 		tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));

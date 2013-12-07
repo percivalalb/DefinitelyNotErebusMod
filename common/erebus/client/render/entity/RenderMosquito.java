da45ef1cd60ba7366d1f9207e6b72998a4c2946f
Vadis365 <curve_linear@hotmail.com>
2013-12-07 21:18:31 +0000

    Mosquito Render file

--- common/erebus/client/render/entity/RenderMosquito.java
+++ common/erebus/client/render/entity/RenderMosquito.java
@@ -11,28 +11,31 @@ import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import erebus.client.model.entity.ModelMosquito;
 import erebus.entity.EntityMosquito;
 
 @SideOnly(Side.CLIENT)
 public class RenderMosquito extends RenderLiving {
+	protected ModelMosquito model;
+	private final ResourceLocation Texture = new ResourceLocation("erebus:textures/mob/Mosquito.png");
 
-	public RenderMosquito(ModelBase par1ModelBase, float par3) {
-		super(par1ModelBase, par3);
-		setRenderPassModel(par1ModelBase);
+	public RenderMosquito(ModelBase par1ModelBase, float par2) {
+		super(par1ModelBase, par2);
+		model = (ModelMosquito) mainModel;
 	}
 
-	public void renderLivingMosquito(EntityMosquito par1EntityMosquito, double par2, double par4, double par6, float par8, float par9) {
+	public void renderMosquito(EntityMosquito par1EntityMosquito, double par2, double par4, double par6, float par8, float par9) {
 		super.doRenderLiving(par1EntityMosquito, par2, par4, par6, par8, par9);
 	}
 
 	@Override
 	public void doRenderLiving(EntityLiving par1EntityLiving, double par2, double par4, double par6, float par8, float par9) {
-		renderLivingMosquito((EntityMosquito) par1EntityLiving, par2, par4, par6, par8, par9);
+		renderMosquito((EntityMosquito) par1EntityLiving, par2, par4, par6, par8, par9);
 	}
 
 	@Override
 	public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9) {
-		renderLivingMosquito((EntityMosquito) par1Entity, par2, par4, par6, par8, par9);
+		renderMosquito((EntityMosquito) par1Entity, par2, par4, par6, par8, par9);
 	}
 
 	@Override
@@ -44,10 +47,8 @@ public class RenderMosquito extends RenderLiving {
 			GL11.glTranslatef(0.0F, 0.0F, 0.5F);
 	}
 
-	private final ResourceLocation resource = new ResourceLocation("erebus:textures/mob/Mosquito.png");
-
 	@Override
 	protected ResourceLocation getEntityTexture(Entity entity) {
-		return resource;
+		return Texture;
 	}
 }

7833e2abbd36a3f25c2dffc6b54af012acd03bc4
Vadis365 <curve_linear@hotmail.com>
2013-12-29 22:05:10 +0000

    Trimmed some useless code From mantis model

--- common/erebus/client/model/entity/ModelPrayingMantis.java
+++ common/erebus/client/model/entity/ModelPrayingMantis.java
@@ -7,8 +7,11 @@ import net.minecraft.util.MathHelper;
 
 import org.lwjgl.opengl.GL11;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import erebus.entity.EntityPrayingMantis;
 
+@SideOnly(Side.CLIENT)
 public class ModelPrayingMantis extends ModelBase
 {
 	ModelRenderer Head1;
@@ -57,283 +60,189 @@ public class ModelPrayingMantis extends ModelBase
 	ModelRenderer LBLeg5;
 	ModelRenderer LBLeg6;
 
-	public ModelPrayingMantis()
-	{
+	public ModelPrayingMantis() {
 		textureWidth = 64;
 		textureHeight = 128;
-
 		Head1 = new ModelRenderer(this, 20, 11);
 		Head1.addBox(-3.5F, -3F, -2.5F, 7, 4, 4);
 		Head1.setRotationPoint(0F, -6F, -12F);
-		Head1.setTextureSize(64, 128);
-		Head1.mirror = true;
 		setRotation(Head1, -0.2617994F, 0F, 0F);
 		Head2 = new ModelRenderer(this, 23, 5);
 		Head2.addBox(-2.5F, 1F, -2.5F, 5, 2, 3);
 		Head2.setRotationPoint(0F, -6F, -12F);
-		Head2.setTextureSize(64, 128);
-		Head2.mirror = true;
 		setRotation(Head2, -0.6108652F, 0F, 0F);
 		Head3 = new ModelRenderer(this, 26, 0);
 		Head3.addBox(-1.5F, 3F, -2F, 3, 2, 2);
 		Head3.setRotationPoint(0F, -6F, -12F);
-		Head3.setTextureSize(64, 128);
-		Head3.mirror = true;
 		setRotation(Head3, -0.6283185F, 0F, 0F);
 		REye = new ModelRenderer(this, 40, 0);
 		REye.addBox(-4F, -4F, -3F, 3, 4, 3);
 		REye.setRotationPoint(0F, -6F, -12F);
-		REye.setTextureSize(64, 128);
-		REye.mirror = true;
 		setRotation(REye, -0.2617994F, 0F, -0.2617994F);
 		LEye = new ModelRenderer(this, 10, 0);
 		LEye.addBox(1F, -4F, -3F, 3, 4, 3);
 		LEye.setRotationPoint(0F, -6F, -12F);
-		LEye.setTextureSize(64, 128);
-		LEye.mirror = true;
 		setRotation(LEye, -0.2617994F, 0F, 0.2617994F);
 		RAnt = new ModelRenderer(this, 0, 99);
 		RAnt.addBox(0F, -11F, -1F, 1, 8, 1);
 		RAnt.setRotationPoint(0F, -6F, -12F);
-		RAnt.setTextureSize(64, 128);
-		RAnt.mirror = true;
 		setRotation(RAnt, 0F, 0F, -0.3490659F);
 		LAnt1 = new ModelRenderer(this, 0, 99);
 		LAnt1.addBox(-1F, -11F, -1F, 1, 8, 1);
 		LAnt1.setRotationPoint(0F, -6F, -12F);
-		LAnt1.setTextureSize(64, 128);
-		LAnt1.mirror = true;
 		setRotation(LAnt1, 0F, 0F, 0.3490659F);
 		Neck = new ModelRenderer(this, 23, 20);
 		Neck.addBox(-2F, -4F, -2.5F, 4, 5, 4);
 		Neck.setRotationPoint(0F, -4F, -8F);
-		Neck.setTextureSize(64, 128);
-		Neck.mirror = true;
 		setRotation(Neck, 0.9599311F, 0F, 0F);
 		Thorax1 = new ModelRenderer(this, 21, 30);
 		Thorax1.addBox(-2.5F, 0F, -3F, 5, 10, 5);
 		Thorax1.setRotationPoint(0F, -4F, -8F);
-		Thorax1.setTextureSize(64, 128);
-		Thorax1.mirror = true;
 		setRotation(Thorax1, 0.6981317F, 0F, 0F);
 		Ab1 = new ModelRenderer(this, 19, 46);
 		Ab1.addBox(-3F, 10F, -4F, 6, 10, 6);
 		Ab1.setRotationPoint(0F, -4F, -8F);
-		Ab1.setTextureSize(64, 128);
-		Ab1.mirror = true;
 		setRotation(Ab1, 0.7330383F, 0F, 0F);
 		Ab2 = new ModelRenderer(this, 16, 63);
 		Ab2.addBox(-4F, 17F, -9F, 8, 12, 7);
 		Ab2.setRotationPoint(0F, -4F, -8F);
-		Ab2.setTextureSize(64, 128);
-		Ab2.mirror = true;
 		setRotation(Ab2, 0.9773844F, 0F, 0F);
 		Ab3 = new ModelRenderer(this, 19, 83);
 		Ab3.addBox(-3F, 29F, -8.5F, 6, 5, 6);
 		Ab3.setRotationPoint(0F, -4F, -8F);
-		Ab3.setTextureSize(64, 128);
-		Ab3.mirror = true;
 		setRotation(Ab3, 0.9773844F, 0F, 0F);
 		Ab4 = new ModelRenderer(this, 23, 95);
 		Ab4.addBox(-2F, 34F, -7F, 4, 2, 4);
 		Ab4.setRotationPoint(0F, -4F, -8F);
-		Ab4.setTextureSize(64, 128);
-		Ab4.mirror = true;
 		setRotation(Ab4, 0.9773844F, 0F, 0F);
 		LArm1 = new ModelRenderer(this, 0, 20);
 		LArm1.addBox(0F, 0F, -1.5F, 2, 10, 3);
 		LArm1.setRotationPoint(2F, -2F, -9F);
-		LArm1.setTextureSize(64, 128);
-		LArm1.mirror = true;
 		setRotation(LArm1, 0.3490659F, 0F, 0F);
 		LArm2 = new ModelRenderer(this, 0, 34);
 		LArm2.addBox(-0.5F, -9F, 4.5F, 3, 10, 3);
 		LArm2.setRotationPoint(2F, -2F, -9F);
-		LArm2.setTextureSize(64, 128);
-		LArm2.mirror = true;
 		setRotation(LArm2, -2.268928F, 0F, 0F);
 		LArm3 = new ModelRenderer(this, 0, 48);
 		LArm3.addBox(0F, 2.5F, -7.5F, 2, 11, 2);
 		LArm3.setRotationPoint(2F, -2F, -9F);
-		LArm3.setTextureSize(64, 128);
-		LArm3.mirror = true;
 		setRotation(LArm3, 0.3490659F, 0F, 0F);
 		LArm4 = new ModelRenderer(this, 0, 62);
 		LArm4.addBox(0F, 3.5F, -8.5F, 2, 8, 1);
 		LArm4.setRotationPoint(2F, -2F, -9F);
-		LArm4.setTextureSize(64, 128);
-		LArm4.mirror = true;
 		setRotation(LArm4, 0.3490659F, 0F, 0F);
 		RArm1 = new ModelRenderer(this, 54, 20);
 		RArm1.addBox(-2F, 0F, -1.5F, 2, 10, 3);
 		RArm1.setRotationPoint(-2F, -2F, -9F);
-		RArm1.setTextureSize(64, 128);
-		RArm1.mirror = true;
 		setRotation(RArm1, 0.3490659F, 0F, 0F);
 		RArm2 = new ModelRenderer(this, 52, 34);
 		RArm2.addBox(-2.5F, -9F, 4.5F, 3, 10, 3);
 		RArm2.setRotationPoint(-2F, -2F, -9F);
-		RArm2.setTextureSize(64, 128);
-		RArm2.mirror = true;
 		setRotation(RArm2, -2.268928F, 0F, 0F);
 		RArm3 = new ModelRenderer(this, 56, 48);
 		RArm3.addBox(-2F, 2.5F, -7.5F, 2, 11, 2);
 		RArm3.setRotationPoint(-2F, -2F, -9F);
-		RArm3.setTextureSize(64, 128);
-		RArm3.mirror = true;
 		setRotation(RArm3, 0.3490659F, 0F, 0F);
 		RArm4 = new ModelRenderer(this, 58, 62);
 		RArm4.addBox(-2F, 3.5F, -8.5F, 2, 8, 1);
 		RArm4.setRotationPoint(-2F, -2F, -9F);
-		RArm4.setTextureSize(64, 128);
-		RArm4.mirror = true;
 		setRotation(RArm4, 0.3490659F, 0F, 0F);
 		RFLeg1 = new ModelRenderer(this, 52, 72);
 		RFLeg1.addBox(-1.5F, 0F, -2F, 3, 5, 3);
 		RFLeg1.setRotationPoint(-2F, 5F, -2F);
-		RFLeg1.setTextureSize(64, 128);
-		RFLeg1.mirror = true;
 		setRotation(RFLeg1, -0.3490659F, -0.7853982F, 0.3490659F);
 		RFLeg2 = new ModelRenderer(this, 56, 81);
 		RFLeg2.addBox(-1F, 5F, -1.5F, 2, 4, 2);
 		RFLeg2.setRotationPoint(-2F, 5F, -2F);
-		RFLeg2.setTextureSize(64, 128);
-		RFLeg2.mirror = true;
 		setRotation(RFLeg2, -0.3490659F, -0.7853982F, 0.3490659F);
 		RFLeg3 = new ModelRenderer(this, 21, 89);
 		RFLeg3.addBox(-9F, 8F, -1F, 9, 2, 1);
 		RFLeg3.setRotationPoint(-2F, 5F, -2F);
-		RFLeg3.setTextureSize(64, 128);
-		RFLeg3.mirror = true;
 		setRotation(RFLeg3, -0.3490659F, -0.7853982F, 0.3490659F);
 		RFLeg4 = new ModelRenderer(this, 60, 99);
 		RFLeg4.addBox(-9F, 10F, -1F, 1, 12, 1);
 		RFLeg4.setRotationPoint(-2F, 5F, -2F);
-		RFLeg4.setTextureSize(64, 128);
-		RFLeg4.mirror = true;
 		setRotation(RFLeg4, -0.3490659F, -0.7853982F, 0.3490659F);
 		RFLeg5 = new ModelRenderer(this, 56, 113);
 		RFLeg5.addBox(-9.5F, 19.5F, -1.5F, 2, 2, 2);
 		RFLeg5.setRotationPoint(-2F, 5F, -2F);
-		RFLeg5.setTextureSize(64, 128);
-		RFLeg5.mirror = true;
 		setRotation(RFLeg5, -0.3490659F, -0.7853982F, 0.3490659F);
 		RFLeg6 = new ModelRenderer(this, 60, 118);
 		RFLeg6.addBox(-10F, 20F, -1F, 1, 4, 1);
 		RFLeg6.setRotationPoint(-2F, 5F, -2F);
-		RFLeg6.setTextureSize(64, 128);
-		RFLeg6.mirror = true;
 		setRotation(RFLeg6, -0.3490659F, -0.7853982F, 0.3490659F);
 		LFLeg1 = new ModelRenderer(this, 0, 72);
 		LFLeg1.addBox(-1.5F, 0F, -2F, 3, 5, 3);
 		LFLeg1.setRotationPoint(2F, 5F, -2F);
-		LFLeg1.setTextureSize(64, 128);
-		LFLeg1.mirror = true;
 		setRotation(LFLeg1, -0.3490659F, 0.7853982F, -0.3490659F);
 		LFLeg2 = new ModelRenderer(this, 0, 81);
 		LFLeg2.addBox(-1F, 5F, -1.5F, 2, 4, 2);
 		LFLeg2.setRotationPoint(2F, 5F, -2F);
-		LFLeg2.setTextureSize(64, 128);
-		LFLeg2.mirror = true;
 		setRotation(LFLeg2, -0.3490659F, 0.7853982F, -0.3490659F);
 		LFLeg3 = new ModelRenderer(this, 21, 89);
 		LFLeg3.addBox(0F, 8F, -1F, 9, 2, 1);
 		LFLeg3.setRotationPoint(2F, 5F, -2F);
-		LFLeg3.setTextureSize(64, 128);
-		LFLeg3.mirror = true;
 		setRotation(LFLeg3, -0.3490659F, 0.7853982F, -0.3490659F);
 		LFLeg4 = new ModelRenderer(this, 0, 99);
 		LFLeg4.addBox(7.95F, 10F, -1F, 1, 12, 1);
 		LFLeg4.setRotationPoint(2F, 5F, -2F);
-		LFLeg4.setTextureSize(64, 128);
-		LFLeg4.mirror = true;
 		setRotation(LFLeg4, -0.3490659F, 0.7853982F, -0.3490659F);
 		LFLeg5 = new ModelRenderer(this, 0, 113);
 		LFLeg5.addBox(7.5F, 19.5F, -1.5F, 2, 2, 2);
 		LFLeg5.setRotationPoint(2F, 5F, -2F);
-		LFLeg5.setTextureSize(64, 128);
-		LFLeg5.mirror = true;
 		setRotation(LFLeg5, -0.3490659F, 0.7853982F, -0.3490659F);
 		LFLeg6 = new ModelRenderer(this, 0, 118);
 		LFLeg6.addBox(9F, 20F, -1F, 1, 4, 1);
 		LFLeg6.setRotationPoint(2F, 5F, -2F);
-		LFLeg6.setTextureSize(64, 128);
-		LFLeg6.mirror = true;
 		setRotation(LFLeg6, -0.3490659F, 0.7853982F, -0.3490659F);
 		RBLeg1 = new ModelRenderer(this, 8, 95);
 		RBLeg1.addBox(-1.5F, 0F, -1.5F, 3, 4, 3);
 		RBLeg1.setRotationPoint(-2F, 8F, 0F);
-		RBLeg1.setTextureSize(64, 128);
-		RBLeg1.mirror = true;
 		setRotation(RBLeg1, 0.3490659F, 0.7853982F, 0.3490659F);
 		RBLeg2 = new ModelRenderer(this, 10, 103);
 		RBLeg2.addBox(-1F, 4F, -1F, 2, 3, 2);
 		RBLeg2.setRotationPoint(-2F, 8F, 0F);
-		RBLeg2.setTextureSize(64, 128);
-		RBLeg2.mirror = true;
 		setRotation(RBLeg2, 0.3490659F, 0.7853982F, 0.3490659F);
 		RBLeg3 = new ModelRenderer(this, 21, 89);
 		RBLeg3.addBox(-9F, 6F, -0.5F, 9, 2, 1);
 		RBLeg3.setRotationPoint(-2F, 8F, 0F);
-		RBLeg3.setTextureSize(64, 128);
-		RBLeg3.mirror = true;
 		setRotation(RBLeg3, 0.3490659F, 0.7853982F, 0.3490659F);
 		RBLeg4 = new ModelRenderer(this, 18, 109);
 		RBLeg4.addBox(-9F, 8F, -0.5F, 1, 10, 1);
 		RBLeg4.setRotationPoint(-2F, 8F, 0F);
-		RBLeg4.setTextureSize(64, 128);
-		RBLeg4.mirror = true;
 		setRotation(RBLeg4, 0.3490659F, 0.7853982F, 0.3490659F);
 		RBLeg5 = new ModelRenderer(this, 14, 121);
 		RBLeg5.addBox(-9.5F, 15.5F, -1F, 2, 2, 2);
 		RBLeg5.setRotationPoint(-2F, 8F, 0F);
-		RBLeg5.setTextureSize(64, 128);
-		RBLeg5.mirror = true;
 		setRotation(RBLeg5, 0.3490659F, 0.7853982F, 0.3490659F);
 		RBLeg6 = new ModelRenderer(this, 9, 120);
 		RBLeg6.addBox(-10F, 16F, -0.5F, 1, 4, 1);
 		RBLeg6.setRotationPoint(-2F, 8F, 0F);
-		RBLeg6.setTextureSize(64, 128);
-		RBLeg6.mirror = true;
 		setRotation(RBLeg6, 0.3490659F, 0.7853982F, 0.3490659F);
 		LBLeg1 = new ModelRenderer(this, 42, 95);
 		LBLeg1.addBox(-1.5F, 0F, -1.5F, 3, 4, 3);
 		LBLeg1.setRotationPoint(2F, 8F, 0F);
-		LBLeg1.setTextureSize(64, 128);
-		LBLeg1.mirror = true;
 		setRotation(LBLeg1, 0.3490659F, -0.7853982F, -0.3490659F);
 		LBLeg2 = new ModelRenderer(this, 44, 103);
 		LBLeg2.addBox(-1F, 4F, -1F, 2, 3, 2);
 		LBLeg2.setRotationPoint(2F, 8F, 0F);
-		LBLeg2.setTextureSize(64, 128);
-		LBLeg2.mirror = true;
 		setRotation(LBLeg2, 0.3490659F, -0.7853982F, -0.3490659F);
 		LBLeg3 = new ModelRenderer(this, 21, 89);
 		LBLeg3.addBox(0F, 6F, -0.5F, 9, 2, 1);
 		LBLeg3.setRotationPoint(2F, 8F, 0F);
-		LBLeg3.setTextureSize(64, 128);
-		LBLeg3.mirror = true;
 		setRotation(LBLeg3, 0.3490659F, -0.7853982F, -0.3490659F);
 		LBLeg4 = new ModelRenderer(this, 40, 109);
 		LBLeg4.addBox(8F, 8F, -0.5F, 1, 10, 1);
 		LBLeg4.setRotationPoint(2F, 8F, 0F);
-		LBLeg4.setTextureSize(64, 128);
-		LBLeg4.mirror = true;
 		setRotation(LBLeg4, 0.3490659F, -0.7853982F, -0.3490659F);
 		LBLeg5 = new ModelRenderer(this, 40, 121);
 		LBLeg5.addBox(7.5F, 15.5F, -1F, 2, 2, 2);
 		LBLeg5.setRotationPoint(2F, 8F, 0F);
-		LBLeg5.setTextureSize(64, 128);
-		LBLeg5.mirror = true;
 		setRotation(LBLeg5, 0.3490659F, -0.7853982F, -0.3490659F);
 		LBLeg6 = new ModelRenderer(this, 49, 120);
 		LBLeg6.addBox(9F, 16F, -0.5F, 1, 4, 1);
 		LBLeg6.setRotationPoint(2F, 8F, 0F);
-		LBLeg6.setTextureSize(64, 128);
-		LBLeg6.mirror = true;
 		setRotation(LBLeg6, 0.3490659F, -0.7853982F, -0.3490659F);
-
-
 	}
 
 	@Override
@@ -394,8 +303,7 @@ public class ModelPrayingMantis extends ModelBase
 		GL11.glPopMatrix();
 	}
 
-	private void setRotation(ModelRenderer model, float x, float y, float z)
-	{
+	private void setRotation(ModelRenderer model, float x, float y, float z) {
 		model.rotateAngleX = x;
 		model.rotateAngleY = y;
 		model.rotateAngleZ = z;
@@ -407,7 +315,6 @@ public class ModelPrayingMantis extends ModelBase
 		float cos1 = MathHelper.cos(par1 + (float) Math.PI) * 0.3F * par2;
 		float cos2 = MathHelper.cos(par1) * 0.3F * par2;
 		float cosxnz1 = MathHelper.cos(par1 * 0.5F + (float) Math.PI) * 0.7F * par2;
-
 		LBLeg1.rotateAngleX = cos1;
 		LBLeg2.rotateAngleX = cos1;
 		LBLeg3.rotateAngleX = cos1;
@@ -415,21 +322,18 @@ public class ModelPrayingMantis extends ModelBase
 		LBLeg5.rotateAngleX = cos1;
 		LBLeg5.rotateAngleX = cos1;
 		LBLeg6.rotateAngleX = cos1;
-
 		LFLeg1.rotateAngleX = cos2;
 		LFLeg2.rotateAngleX = cos2;
 		LFLeg3.rotateAngleX = cos2;
 		LFLeg4.rotateAngleX = cos2;
 		LFLeg5.rotateAngleX = cos2;
 		LFLeg6.rotateAngleX = cos2;
-
 		RBLeg1.rotateAngleX = cos2;
 		RBLeg2.rotateAngleX = cos2;
 		RBLeg3.rotateAngleX = cos2;
 		RBLeg4.rotateAngleX = cos2;
 		RBLeg5.rotateAngleX = cos2;
 		RBLeg6.rotateAngleX = cos2;
-
 		RFLeg1.rotateAngleX = cos1;
 		RFLeg2.rotateAngleX = cos1;
 		RFLeg3.rotateAngleX = cos1;
@@ -442,17 +346,14 @@ public class ModelPrayingMantis extends ModelBase
 			LArm2.rotateAngleX = cosxnz1 - 2.268928F;
 			LArm3.rotateAngleX = cosxnz1 + 0.3490659F;
 			LArm4.rotateAngleX = cosxnz1 + 0.3490659F;
-
 			RArm1.rotateAngleX = -cosxnz1 + 0.3490659F;
 			RArm2.rotateAngleX = -cosxnz1 - 2.268928F;
 			RArm3.rotateAngleX = -cosxnz1 + 0.3490659F;
 			RArm4.rotateAngleX = -cosxnz1 + 0.3490659F;
-
 			LArm1.rotateAngleY = 0.0F;
 			LArm2.rotateAngleY = 0.0F;
 			LArm3.rotateAngleY = 0.0F;
 			LArm4.rotateAngleY = 0.0F;
-
 			RArm1.rotateAngleY = 0.0F;
 			RArm2.rotateAngleY = 0.0F;
 			RArm3.rotateAngleY = 0.0F;
@@ -461,25 +362,21 @@ public class ModelPrayingMantis extends ModelBase
 
 		if (entityPrayingMantis.getDataWatcher().getWatchableObjectByte(22) == 0) {
 			LArm1.rotateAngleX = -0.873F;
-			LArm2.rotateAngleX = +2.793F;
+			LArm2.rotateAngleX = 2.793F;
 			LArm3.rotateAngleX = -0.873F;
 			LArm4.rotateAngleX = -0.873F;
-
 			RArm1.rotateAngleX = -0.873F;
-			RArm2.rotateAngleX = +2.793F;
+			RArm2.rotateAngleX = 2.793F;
 			RArm3.rotateAngleX = -0.873F;
 			RArm4.rotateAngleX = -0.873F;
-
 			LArm1.rotateAngleY = -1.0F;
 			LArm2.rotateAngleY = -1.0F;
 			LArm3.rotateAngleY = -1.0F;
 			LArm4.rotateAngleY = -1.0F;
-
-			RArm1.rotateAngleY = +1.0F;
-			RArm2.rotateAngleY = +1.0F;
-			RArm3.rotateAngleY = +1.0F;
-			RArm4.rotateAngleY = +1.0F;
+			RArm1.rotateAngleY = 1.0F;
+			RArm2.rotateAngleY = 1.0F;
+			RArm3.rotateAngleY = 1.0F;
+			RArm4.rotateAngleY = 1.0F;
 		}
-
 	}
 }

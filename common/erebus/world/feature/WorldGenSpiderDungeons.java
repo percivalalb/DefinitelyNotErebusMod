f44acb1aeb8d1d0c3f2d7538d0c7f9c72b8eb283
Vadis365 <curve_linear@hotmail.com>
2014-01-16 23:42:29 +0000

    Made Web Slinger dungeon loot for now...

--- common/erebus/world/feature/WorldGenSpiderDungeons.java
+++ common/erebus/world/feature/WorldGenSpiderDungeons.java
@@ -33,7 +33,8 @@ public class WorldGenSpiderDungeons extends WorldGenerator {
 	new LootItemStack(ModItems.erebusMaterials).setAmount(2, 6).setDamage(ItemErebusMaterial.dataCompoundEyes).setWeight(7),
 	new LootItemStack(ModItems.erebusMaterials).setAmount(1, 1).setDamage(ItemErebusMaterial.dataCompoundLens).setWeight(2),
 	new LootItemStack(ModItems.maxSpeedBow).setAmount(1).setWeight(1),
-	new LootItemStack(ModBlocks.umberGolemStatue).setAmount(1).setWeight(1)
+	new LootItemStack(ModBlocks.umberGolemStatue).setAmount(1).setWeight(1),
+	new LootItemStack(ModItems.webSlinger).setAmount(1).setWeight(1)
 	);
 	//@formatter:on
 

c084f41bea9810db776756318333f8bea73a303a
ganymedes01 <foka_12@hotmail.com>
2014-01-20 23:14:12 +0000

    Adjusting slots to new gui size

--- common/erebus/inventory/ContainerExtenderThingy.java
+++ common/erebus/inventory/ContainerExtenderThingy.java
@@ -19,9 +19,9 @@ public class ContainerExtenderThingy extends Container {
 
 		for (int i = 0; i < 3; i++)
 			for (int j = 0; j < 9; j++)
-				addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 56 + i * 18));
+				addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 54 + i * 18));
 		for (int i = 0; i < 9; i++)
-			addSlotToContainer(new Slot(inventory, i, 8 + i * 18, 114));
+			addSlotToContainer(new Slot(inventory, i, 8 + i * 18, 112));
 	}
 
 	@Override

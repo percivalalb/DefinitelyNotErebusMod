7198b98a67aac88a342d00e3cf8645ddac01e71e
Vadis365 <curve_linear@hotmail.com>
2014-01-21 19:49:32 +0000

    Temporary Textures

--- common/erebus/block/BlockExtenderThingy.java
+++ common/erebus/block/BlockExtenderThingy.java
@@ -3,16 +3,22 @@ package erebus.block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.BlockPistonBase;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import erebus.ErebusMod;
 import erebus.core.proxy.CommonProxy;
 import erebus.tileentity.TileEntityExtenderThingy;
 
 public class BlockExtenderThingy extends BlockContainer {
+	@SideOnly(Side.CLIENT)
+	private Icon iconTop, iconBottom;
 
 	public BlockExtenderThingy(int id) {
 		super(id, Material.wood);
@@ -43,5 +49,19 @@ public class BlockExtenderThingy extends BlockContainer {
 	@Override
 	public TileEntity createNewTileEntity(World world) {
 		return new TileEntityExtenderThingy();
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int side, int meta) {
+		return side == 0 ? iconBottom : side == 1 ? iconTop : blockIcon;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister iconRegister) {
+		blockIcon = iconRegister.registerIcon("erebus:extenderThingySide");// Side
+		iconTop = iconRegister.registerIcon("erebus:extenderThingyTop");// Top
+		iconBottom = iconRegister.registerIcon("erebus:extenderThingyBot");
 	}
 }
